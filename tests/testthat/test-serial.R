71216ce71323b5c81fbfa1ba40463fb91396813a
Lionel Henry <lionel.hry@gmail.com>
2017-07-13 15:05:07 +0200

    Test for serialised search path

--- tests/testthat/test-serial.R
+++ tests/testthat/test-serial.R
@@ -19,3 +19,24 @@ test_that("can trace the search path of closures", {
   path_names <- map_chr(path, attr, which = "name")
   expect_identical(path_names, c("utils", "base"))
 })
+
+
+roundtrip <- function(x) {
+  bytes_unserialise(serialise_bytes(x))
+}
+
+test_that("relevant packages are correctly roundtripped", {
+  clo <- with_env(global_env(), local({
+    foo <- "foo"
+    # Refers to rlang, utils, and local env
+    function() { ll(foo, apropos) }
+  })
+  )
+  out <- roundtrip(clo)
+  expect_identical(out(), list("foo", utils::apropos))
+
+  parents <- env_parents(out)
+  expect_true(every(parents[1:3], is_package_env))
+  expect_identical(map_chr(parents[1:2], attr, which = "name"), c("package:rlang", "package:utils"))
+  expect_true(is_reference(parents[[3]], base_env()))
+})
